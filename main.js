5108a4a52858c015c143ce0fb4f34632d771ded4
IgnacioDemarcico <ignaciodemarcico@gmail.com>
2023-04-23 12:18:51 -0300

    tp terminado

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var datos
+
 const cargarProductos = async () => {
     const response = await fetch('https://dummyjson.com/products/')
     const data = await response.json()
@@ -7,12 +8,10 @@ const cargarProductos = async () => {
     const container = document.getElementById("test1")
     for(producto of data.products) {
         container.innerHTML += `
-        <div id="seccion-${producto.title}"class="card">
-        <img src="${producto.thumbnail}" class="card-img-top" alt="...">
+        <div id="seccion-${producto.title}"class="card"  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="verDetalles(${producto.id})">
+        <img src="${producto.thumbnail}" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">${producto.title}</h5>
-          <p class="card-text">${producto.description}</p>
-        </div>
       </div>
         `
     }
@@ -35,4 +34,16 @@ const dispararOp = () => {
     document.getElementById("killsound").play()
     document.getElementById("kill").classList.remove("d-none")
     setTimeout(() => document.getElementById("kill").classList.add("d-none"), 2.1 * 1000)
+}
+const verDetalles = (id) =>{
+    let producto
+    console.log(datos)
+    for(let dato of datos.products){
+        if(dato.id == id){
+            producto  =  dato
+        }
+    }
+    if(!producto)return    
+    document.getElementById("modaltitulo").innerHTML=producto.title
+    document.getElementById("modalbody").innerHTML=producto.description
 }
